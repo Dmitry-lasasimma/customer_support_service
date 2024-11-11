732e732cf610c6364688176db01278cb6989b61d
dim.lasasimma.12 <dim.lasasimma.12@gmail.com>
2024-11-11 17:10:30 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use("/v1/api/theme", themeRoutes);
 const PORT = process.env.PORT || 9000;
 
 // Start the server
-//test
+//test01
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
